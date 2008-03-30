282da086cdd04217800723c092355199e62a6e7f
tim.visher <tim.visher@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-03-30 14:47:48 +0000

    Initial Import.

---
+++ trunk/CHCHelpDeskServices/ejbModule/edu/chc/helpdesk/email/MailMessage.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package edu.chc.helpdesk.email;
+
+
+/**
+ * @author tim
+ *
+ */
+public class MailMessage implements Message {
+   
+   public boolean send() {
+      return false;
+   }
+
+}
