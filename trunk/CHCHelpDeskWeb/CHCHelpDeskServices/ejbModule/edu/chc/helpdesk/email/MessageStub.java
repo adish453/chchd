f0cbea2265766deb20467596bea6306e728cf5dc
hgorm86@comcast.net <hgorm86@comcast.net@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-04-03 14:22:59 +0000

    Initial import.

---
+++ trunk/CHCHelpDeskWeb/CHCHelpDeskServices/ejbModule/edu/chc/helpdesk/email/MessageStub.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.chc.helpdesk.email;
+
+
+/**
+ * @author tim
+ *
+ */
+public class MessageStub implements Message {
+
+   /**
+    * @see edu.chc.helpdesk.email.Message#send()
+    */
+   public boolean send() {
+      return true;
+   }
+
+}
