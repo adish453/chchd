e4a1bbb5840d092d3235ebb7de12982ce4cbc531
tim.visher <tim.visher@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-04-13 20:39:56 +0000

    Stubbed out some tests and created the EJB HelpRequest Service Implementation class skeletons.

---
+++ CHCHelpDeskSystem/ejbModule/edu/chc/helpdesk/requests/EJBHelpRequestServiceStub.java
@@ -0,0 +1,10 @@
+package edu.chc.helpdesk.requests;
+
+public class EJBHelpRequestServiceStub implements HelpRequestService {
+
+	public HelpRequest submit(HelpRequest request) throws HelpRequestNotValidException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CHCHelpDeskSystem/tests/edu/chc/helpdesk/requests/EJBHelpRequestServiceTest.java
@@ -0,0 +1,14 @@
+package edu.chc.helpdesk.requests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EJBHelpRequestServiceTest {
+
+	@Test
+	public void testSubmit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
