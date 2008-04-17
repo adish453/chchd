45cef367fc0c99a6b0bceec4ce69933a05dfd7c2
tim.visher <tim.visher@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-04-17 14:25:42 +0000

    Finished stubbing out the request system.

--- CHCHelpDeskSystem/ejbModule/edu/chc/helpdesk/requests/EJBHelpRequestServiceStub.java
+++ CHCHelpDeskSystem/ejbModule/edu/chc/helpdesk/requests/EJBHelpRequestServiceStub.java
@@ -2,15 +2,9 @@ package edu.chc.helpdesk.requests;
 
 public class EJBHelpRequestServiceStub implements HelpRequestService {
 	
-	boolean success;
-	
-	public EJBHelpRequestServiceStub(boolean success) {
-		this.success = success;
-	}
-
 	public HelpRequest submit(HelpRequest request) throws HelpRequestNotValidException {
 		// TODO Auto-generated method stub
-		return null;
+		return new HelpRequestStub();
 	}
 
 }
