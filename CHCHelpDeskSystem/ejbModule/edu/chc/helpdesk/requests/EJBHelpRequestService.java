87428d0ed85377240255de61b978f9f079a5e261
tim.visher <tim.visher@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-05-01 15:10:06 +0000

    Again!

--- CHCHelpDeskSystem/ejbModule/edu/chc/helpdesk/requests/EJBHelpRequestService.java
+++ CHCHelpDeskSystem/ejbModule/edu/chc/helpdesk/requests/EJBHelpRequestService.java
@@ -1,19 +1,28 @@
 package edu.chc.helpdesk.requests;
 
-import java.io.*;
 import javax.ejb.Stateless;
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
+import edu.chc.helpdesk.email.Message;
+import edu.chc.helpdesk.email.MessageNotValidException;
+import edu.chc.helpdesk.email.MessageType;
 
 @Stateless
 public class EJBHelpRequestService implements HelpRequestService {
 
-	// injected resource for database access
-	@PersistenceContext(name="CHCHelpDeskDB")
-	private EntityManager em;
-
 	// Should this throw HelpRequestNotValidException as a checked exception? 
 	// Maybe the presentation layer should handle the exception by mapping
 	// exceptions to error pages in faces-config.xml
+<<<<<<< .mine
+	public void submit(HelpRequest request) throws HelpDeskApplicationException {
+
+		if (!request.isValid()) {
+			throw new HelpRequestNotValidException();
+		}
+		try {
+=======
 	public HelpRequest submit(HelpRequest request) 
 		throws HelpDeskApplicationException
 	{
@@ -23,24 +32,30 @@ public class EJBHelpRequestService implements HelpRequestService {
 		
 		try
 		{
+>>>>>>> .r140
 			em.persist(request);
-			return request;
-		}
-		catch(PersistenceException pe)
-		{
-			//we should log the real exception somewhere,
-			//otherwise debugging will be *really* hard
-			System.out.print(pe.getMessage());
+		} catch (PersistenceException pe) {
+			//TODO Use the Logging API
 			throw new HelpRequestSaveException("Request was not submitted due to a database error.");
-		}
-		catch(Exception e)
-		{
-			//we should log the real exception somewhere,
-			//otherwise debugging will be *really* hard
-			System.out.print(e.getMessage());
+		} catch (Exception e) {
+			//TODO Use the Logging API
 			throw new HelpRequestSaveException("Request was not submitted. A fatal error occurred.");
+		}
+		
+		Message msg = new Message(request);
+		
+		try {
+			msg.send(MessageType.CUSTOMER);
+			msg.send(MessageType.TECH);
+		} catch (MessageNotValidException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
-	
+
+	// injected resource for database access
+	@PersistenceContext(name = "CHCHelpDeskDB")
+	private EntityManager em;
+
 }
