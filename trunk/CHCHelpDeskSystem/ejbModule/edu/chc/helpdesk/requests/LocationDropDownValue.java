16f9e2d11360eaba9c0ea3dff4bba141cd81fdcb
mgspross <mgspross@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-05-06 03:40:50 +0000

    New drop-down classes..w00t!

---
+++ trunk/CHCHelpDeskSystem/ejbModule/edu/chc/helpdesk/requests/LocationDropDownValue.java
@@ -0,0 +1,20 @@
+package edu.chc.helpdesk.requests;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema="HelpDeskDB")
+@DiscriminatorValue("LOCATION")
+public class LocationDropDownValue extends DropDownValue {
+	/* This class is a placeholder.
+	 * It is declared so that the rest of the
+	 * code can refer to Location drop-down values
+	 * in an object-oriented manner and use
+	 * ploymorphic queries.
+	 * Like all drop-downs, it only uses
+	 * the inherited ID and displayValue columns
+	 * and has no "extra" functionality.
+	 */
+}
