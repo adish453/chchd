61f7424569f94a928a43f5a630dfd50c0b2cfb92
mgspross <mgspross@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-05-12 06:07:29 +0000

    * Made some changes to get it to run on my machine (it won't run anywhere else right now)
    * Fixed bean code

--- CHCHelpDeskAdminClient/src/edu/chc/helpdesk/admin/BrowseRequestBean.java
+++ CHCHelpDeskAdminClient/src/edu/chc/helpdesk/admin/BrowseRequestBean.java
@@ -1,54 +1,97 @@
 package edu.chc.helpdesk.admin;
 
-public class BrowseRequestBean {
-	String firstName, lastName, email, location, phoneNumber, room;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.ejb.EJB;
+import javax.faces.model.SelectItem;
+
+import edu.chc.helpdesk.requests.DropDownValue;
+import edu.chc.helpdesk.requests.HelpRequestService;
+
+public class BrowseRequestBean {
+	
+	@EJB
+	protected HelpRequestService requestService;
+	
+	private String firstName;
+	private String lastName;
+	private String roomNo;
+	
+	private int location;
+	private int problem;
+	
+	private List locationSelectItems;
+	private List issueSelectItems;
+	
+	/**
+	 * @return the firstName
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * @param firstName the firstName to set
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * @return the lastName
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * @param lastName the lastName to set
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-	public String getEmail() {
-		return email;
+	/**
+	 * @return the roomNo
+	 */
+	public String getRoomNo() {
+		return roomNo;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	/**
+	 * @param roomNo the roomNo to set
+	 */
+	public void setRoomNo(String roomNo) {
+		this.roomNo = roomNo;
 	}
 
-	public String getLocation() {
+	/**
+	 * @return the location
+	 */
+	public int getLocation() {
 		return location;
 	}
 
-	public void setLocation(String location) {
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(int location) {
 		this.location = location;
 	}
 
-	public String getPhoneNumber() {
-		return phoneNumber;
+	/**
+	 * @return the problem
+	 */
+	public int getProblem() {
+		return problem;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getRoom() {
-		return room;
-	}
-
-	public void setRoom(String room) {
-		this.room = room;
+	/**
+	 * @param problem the problem to set
+	 */
+	public void setProblem(int problem) {
+		this.problem = problem;
 	}
 	
 	public String search()
@@ -58,5 +101,38 @@ public class BrowseRequestBean {
 	
 	public String reset(){
 		return "reset";
+	}
+	
+	public List getLocationSelectItems() {
+		if(locationSelectItems ==  null) {
+			locationSelectItems = getSelectItemsForList(requestService.getLocationDropDownList());
+		}
+		return locationSelectItems;
+	}
+	
+	public void setLocationSelectItems(List locationSelectItems)  {
+		this.locationSelectItems = locationSelectItems;
+	}
+	
+	public List getIssueSelectItems() {
+		if(issueSelectItems == null){
+			issueSelectItems = getSelectItemsForList(requestService.getIssueDropDownList());
+		}
+		return issueSelectItems;
+	}
+	
+	public void setIssueSelectItems(List issueSelectItems)  {
+		this.issueSelectItems = issueSelectItems;
+	}
+	
+	private List getSelectItemsForList(List<? extends DropDownValue> valueList) {
+		
+		List selectItems = new ArrayList();
+		
+		for(DropDownValue value : valueList) {
+			selectItems.add(new SelectItem(value.getID(),value.getDisplayValue()));
+		}
+		
+		return selectItems;
 	}
 }
