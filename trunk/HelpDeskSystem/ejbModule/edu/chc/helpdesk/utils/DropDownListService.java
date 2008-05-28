4bf7811a339a2dfd6ce68211c86ea3a24d210be0
tim.visher <tim.visher@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-05-28 18:41:11 +0000

    Created a successful JDBC connection using hard-coded values.

--- trunk/HelpDeskSystem/ejbModule/edu/chc/helpdesk/utils/DropDownListService.java
+++ trunk/HelpDeskSystem/ejbModule/edu/chc/helpdesk/utils/DropDownListService.java
@@ -1,29 +1,43 @@
 package edu.chc.helpdesk.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.chc.helpdesk.requests.DropDownValue;
 
 /**
- * 
- * @author tvishe01
- * 
- * Service for encapsulating JDBC calls to the DropDownValue table for populating 
- * GUI Drop Down Lists and Updating their values in the DB.
- *
+ * @author tvishe01 Service for encapsulating JDBC calls to the DropDownValue
+ *         table for populating GUI Drop Down Lists and Updating their values in
+ *         the DB.
  */
 
 public class DropDownListService {
-
-	public List<DropDownValue> getIssueDropDownList() {
-		List<DropDownValue> issueList = new ArrayList<DropDownValue>();
-		//TODO setup a jdbc connection the JNDI available HelpDeskDS
-		//TODO populate issueList with the result set of the following SQL statement
-			// select DISPLAYVALUE from DROPDOWNVALUE where LISTNAME is ISSUE;
-		return issueList;
-	}
-	
-	
-
+    
+    public List<DropDownValue> getIssueDropDownList() {
+        List<DropDownValue> issueList = new ArrayList<DropDownValue>();
+        // TODO utilize JNDI available HelpDeskDS
+        String driver = "org.apache.derby.jdbc.ClientDriver";
+        String dbName = "HelpDeskDB";
+        String connectionURL = "jdbc:derby://localhost:1527/" + 
+            dbName + ";user=HelpDeskApp;password=HelpDeskApp;create=true";
+        
+        try {
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(connectionURL);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // TODO populate issueList with the result set of the following SQL
+        // statement
+        // select DISPLAYVALUE from DROPDOWNVALUE where LISTNAME is ISSUE;
+        return issueList;
+    }
+    
 }
