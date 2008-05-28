a749939c53c60d29874c4b73d61be7d7213d1af0
tim.visher <tim.visher@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-05-28 21:14:07 +0000

    - Deprecated EJBHelpRequestService.getIssueByID(int) - Implemented DropDownListService.getIssuebyID(int) - Updated master.sql.ddl to run the full gamut of DB set up.  This can be safely run from within ij - Fixed an SQL error in populate-dropdownvalue.sql - Updated DropDownListServiceTest to test the new method. - Various and Sundry small edits.

--- HelpDeskSystem/populate-dropdownvalue.sql
+++ HelpDeskSystem/populate-dropdownvalue.sql
@@ -1,14 +1,14 @@
 -- Bad. Does not let you source it from the Command Line.
 -- CONNECT 'jdbc:derby://localhost:1527/HelpDeskDB;user=HelpDeskApp;password=HelpDeskApp;create=true';
 
-DELETE FROM DropDownValue;
+DROP TABLE DropDownValue;
 
-INSERT INTO DropDownValue VALUES 
-(1, 'ISSUE', 'E-Mail'),
-(2, 'ISSUE', 'Outlook'),
-(3, 'ISSUE', 'Powerpoint'),
-(4, 'LOCATION', 'Martino Hall'),
-(5, 'LOCATION', 'Saint Josephs'),
-(6, 'LOCATION', 'Fournier'),
-(7, 'STATUS', 'Open'),
-(8, 'STATUS', 'Closed');
+INSERT INTO DropDownValue(ListName, DisplayValue) VALUES 
+('ISSUE', 'E-Mail'),
+('ISSUE', 'Outlook'),
+('ISSUE', 'Powerpoint'),
+('LOCATION', 'Martino Hall'),
+('LOCATION', 'Saint Josephs'),
+('LOCATION', 'Fournier'),
+('STATUS', 'Open'),
+('STATUS', 'Closed');
