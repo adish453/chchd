24dc3006b9b6f1b2217f60433dd47755da18a823
mgspross <mgspross@d1d67c10-c84e-0410-a23d-cf72bbd316f7>
2008-05-30 05:10:38 +0000

    * Removed derbyclient.jar from the classpath, as this setting is platform-specific as far as I can tell (the path will be wrong on other systems). Instead, we should install the Derby Core and UI plug-ins for Eclipse, which can be downloaded from here:
    http://db.apache.org/derby/releases/release-10.4.1.3.cgi
    
    * Added Derby Nature to the project to get Derby Libraries onto the classpath in a (hopefully) platform neutral way.
    
    * Expression.java: Minor edits. Wasn't escaping single-quotes in string literals, which would have caused problems in SQL statements (injection attacks, yadda, yadda...)
    
    * master.ddl: Eclipse's built-in SQL parser does not like the multi-row INSERT INTO syntax (even though it works in ij). Created a separate INTO INTO statement for each row in the DropDownValue table.

--- trunk/HelpDeskSystem/src/edu/chc/helpdesk/utils/Expression.java
+++ trunk/HelpDeskSystem/src/edu/chc/helpdesk/utils/Expression.java
@@ -7,42 +7,42 @@ import java.util.Date;
  * 
  * @author mspross
  *
- * A simple utility class for building expressions that can be used
- * to build SQL WHERE clauses
+ * A simple utility class for constructing expressions which can be used
+ * to generate criteria clauses in SQL queries.
  */
 public class Expression {
 	
 	public static String equalTo(String field, Object value) {
-		return relation(field,"=",value);
+		return makeRelation(field,"=",value);
 	}
 	
 	public static String notEqualTo(String field, Object value) {
-		return relation(field,"<>",value);
+		return makeRelation(field,"<>",value);
 	}
 	
 	public static String like(String field, Object value) {
 		String s = value.toString();
 		s=s.replace("*", "%");
-		return relation(field,"LIKE",s);
+		return makeRelation(field,"LIKE",s);
 	}
 	
 	public static String greaterThan(String field, Object value) {
-		return relation(field,">",value);
+		return makeRelation(field,">",value);
 	}
 	
 	public static String lessThan(String field, Object value) {
-		return relation(field,"<",value);
+		return makeRelation(field,"<",value);
 	}
 	
 	public static String greaterThanOrEqual(String field, Object value) {
-		return relation(field,">=",value);
+		return makeRelation(field,">=",value);
 	}
 	
 	public static String lessThanOrEqual(String field, Object value) {
-		return relation(field,"<=",value);
+		return makeRelation(field,"<=",value);
 	}
 	
-	protected static String relation(String field, String operator, Object value) {
+	protected static String makeRelation(String field, String operator, Object value) {
 		
 		StringBuilder sb = new StringBuilder();
 		sb.append(field);
@@ -58,16 +58,42 @@ public class Expression {
 		
 	}
 	
+	/**
+	 * @param value the value to delimit
+	 * @return the value passed in, properly delimited for use in a SQL query
+	 * */
 	protected static String getDelimiter(Object value) {
+		
+		//Decide how to delimit 'value' based on its type.
+		//Strings and dates need to be delimited with single-quotes in SQL.
+		//Other fields don't need a delimiter
+		
 		if(value instanceof String || value instanceof Date)
 			return "'";
 		else
 			return "";
 	}
 	
+	/**
+	 * 
+	 * @param value the value to format
+	 * @return a formatted version of 'value'
+	 * 
+	 * This method formats a value so that it will be
+	 * valid in a SQL query. This function makes sure
+	 * special characters are escaped properly in strings
+	 * and ensures that all values of the same object type
+	 * are formatted in a similar manner for consistency
+	 * in generated expressions.
+	 */
 	protected static String format(Object value) {
 		
-		if(value instanceof Date) {
+		if(value instanceof String) {
+			// escape single quotes in strings
+			String s = value.toString();
+			return s.replace("'", "''");
+		}
+		else if(value instanceof Date) {
 			SimpleDateFormat formatter = new SimpleDateFormat("M/d/yyyy hh:mm");
 			return formatter.format(value);
 		}
